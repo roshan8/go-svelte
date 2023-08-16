301d2cad6a5af5069e5baaf25d41e928371f6c54
Roshan shetty <roshan.aloor@gmail.com>
2023-08-16 22:51:57 +0530

    create backend app using go

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-
+## FE
 1. Create svelte app
 ```sh
 npm create svelte@latest fe  # basic skeleton app with typescript enabled
 cd fe
 npm install
 ```
+
+2. Start the svelte app
+```sh
+HOST=0.0.0.0 PORT=9000 npm run dev
+```
+
+
+## BE
---
+++ be/main.go
@@ -0,0 +1,41 @@
+// main.go
+
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func main() {
+	startServer()
+}
+
+func startServer() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		data := map[string]interface{}{
+			"name": "Roshan",
+		}
+		jData, _ := json.Marshal(data)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(jData)
+	})
+	http.ListenAndServe(":8080", r)
+}
